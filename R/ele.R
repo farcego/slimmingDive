bd615238cfe5f5fbb8416fd571123b61a0814280
farcego <Fernando.ArceGonzalez@utas.edu.au>
2019-06-23 20:06:09 +1000

    data help modified

---
+++ R/ele.R
@@ -0,0 +1,13 @@
+##' Pre-processed seal
+##'
+##' An example of a seal dataset as tipically provided by SMRU tags
+##'
+##' @format A data frame with 53940 rows and 10 variables:
+##' \describe{
+##'   \item{ref}{id of the tag}
+##'   \item{DE_DATE}{Date of each dive}
+##'   \item{SURF_DUR}{Duration of the surface post-dive period}
+##' ...
+##' }
+##' @source \url{http:utas.edu.au}
+"ele"
