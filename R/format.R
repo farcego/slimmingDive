84bdeb3276163519b48156e1337d1e3a1a081d2b
farcego <Fernando.ArceGonzalez@utas.edu.au>
2019-06-23 16:03:59 +1000

    formatDives added

---
+++ R/format.R
@@ -0,0 +1,34 @@
+##' function that makes a subset of variables with some recoding and dive removal for filtering purposes
+##'
+##' This functions keeps the variables needed to apply the subsequents procedures of this package.
+##' It will make the date of class POSIXct if supplied as a factor and will make the ref (id of the tag) of type character. it also sort the rows by tag id (ref) and by date (Date).
+##' It also removes dives shallower than 100 meter depth and shorter than 300 seconds.
+##' @title formatDives
+##' @param Data an object of class daata.frame
+##' @return a data.frame containing a subset of the variables needed to keep processing the data.frame
+formatDives <- function(Data){
+    '%out%' <- Negate('%in%')
+    Data <- Data[,c("ref", "DE_DATE", "SURF_DUR", "DIVE_DUR","MAX_DEP","D1","D2",
+                    "D3", "D4","T1", "T2","T3","T4")]
+    if ('POSIXct' %out% class(Data$DE_DATE)){
+        Data$Date <- as.POSIXct(strptime(as.character(Data$DE_DATE), format = '%d/%m/%y %H:%M:%S'))
+    } else {
+        Data$Date <- Data$DE_DATE
+    }
+    Data$DE_DATE <- NULL
+    Data$ref <- as.character(Data$ref)
+    Data$t1 <- Data$T1
+    Data$t2 <- Data$T2
+    Data$t3 <- Data$T3
+    Data$t4 <- Data$T4
+    Data$T1 <- (Data$t1*Data$DIVE_DUR) / 100
+    Data$T2 <- (Data$t2*Data$DIVE_DUR) / 100
+    Data$T3 <- (Data$t3*Data$DIVE_DUR) / 100
+    Data$T4 <- (Data$t4*Data$DIVE_DUR) / 100
+    Data <- split(Data, Data$ref)
+    Data <- lapply(Data, function(fo) fo[order(fo$Date), ])
+    Data <- do.call(rbind, Data)
+    Data <- Data[Data$DIVE_DUR > 300 & Data$MAX_DEP > 100
+               & Data$T1 > 0, ]
+    return(Data)
+}
