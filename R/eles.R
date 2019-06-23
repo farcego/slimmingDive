f7693c844da901e2caf0dc03ebea3a280329699c
farcego <Fernando.ArceGonzalez@utas.edu.au>
2019-06-23 19:44:00 +1000

    data files updated

---
+++ R/eles.R
@@ -0,0 +1,12 @@
+##' Post-processed example (for saving computing time on the vignette)
+##'
+##' An example of a seal after being fully processed by the package
+##'
+##' @format A data frame with 53940 rows and 10 variables:
+##' \describe{
+##'   \item{Date}{Drift rate}
+##'   \item{NDE}{timestamp for each dive}
+##'   \item{zetas}{probability of being inside the drifting trajectory}
+##' }
+##' @source \url{http://www.diamondse.info/}
+"eleK"
