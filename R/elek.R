2b78090c36c8dbdac53d6af184e420e4641339c5
farcego <Fernando.ArceGonzalez@utas.edu.au>
2019-06-24 13:11:11 +1000

    some fixes. It passes cran with two notes

---
+++ R/elek.R
@@ -0,0 +1,16 @@
+##' Post-processed seal
+##'
+##' An example of a seal dataset as tipically provided by SMRU tags
+##'
+##' @format A list with 6 objects:
+##' \describe{
+##'   \item{Data}{Dataset imputed to the kalman filter}
+##'   \item{model}{model definition}
+##'   \item{kalman}{kalman expression}
+##'   \item{mns}{summaries}
+##'   \item{duration}{length of the run}
+##'   \item{burn.in}{burn.in length, in iterations}
+##' ...
+##' }
+##' @source \url{http://www.imos.org.au}
+"elek"
