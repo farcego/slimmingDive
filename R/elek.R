df3f806a7ff7faf257086b0d0006c552b31d4de0
Fer <farcego@gmail.com>
2020-09-05 09:48:36 +1000

    help file improved

--- R/elek.R
+++ R/elek.R
@@ -1,11 +1,14 @@
 ##' Post-processed seal
 ##'
-##' The seal dataset (ele) provided in slimmingDive after being fully
-##' processed by slimmingDive (before aplying the postKalman function)
+##' The original seal dataset (ele) provided in slimmingDive after
+##' being fully processed by slimmingDive (just before aplying the
+##' postKalman function). This data set will save time for testing or
+##' experimenting by avoiding the use of the time consuming
+##' |code{kalman} function.
 ##' 
 ##' @format A list with 6 objects:
 ##' \describe{
-##'   \item{Data}{Dataset imputed to the kalman filter}
+##'   \item{Data}{Dataset imputed to the \code{kalman} function}
 ##'   \item{model}{model definition}
 ##'   \item{kalman}{kalman expression}
 ##'   \item{mns}{summaries}
@@ -14,4 +17,5 @@
 ##' ...
 ##' }
 ##' @source \url{http://www.imos.org.au}
+##' @export
 "elek"
