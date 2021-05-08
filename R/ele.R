d529fb906c8434e756ae36dcb1e2306c0f719388
Fer <farcego@gmail.com>
2021-05-09 08:37:33 +1000

    variables properly ordered

--- R/ele.R
+++ R/ele.R
@@ -1,13 +1,19 @@
-##' elephant seal pre-processed dataset
+##' elephant seal pre-processed dataset (subset)
 ##'
 ##' An example of a seal dataset as tipically provided by SMRU
 ##' tags. There are slight differences between datasets from SMRU
 ##' tags. Some variables may be lower or upper case in different data
-##' sets.
+##' sets. It is a subset containing only the variables tipically
+##' needed to process summarized dives in slimmingDive.
 ##'
 ##' @format A data frame with 53940 rows and 10 variables:
 ##' \describe{
-
+##' 
+##'   \item{ref}{ref}
+##'   \item{DE_DATE}{DE_DATE}
+##'   \item{SURF_DUR}{SURF_DUR}
+##'   \item{DIVE_DUR}{DIVE_DUR}
+##'   \item{MAX_DEP}{MAX_DEP}
 ##'   \item{D1}{D1}
 ##'   \item{D2}{D2}
 ##'   \item{D3}{D3}
@@ -16,13 +22,8 @@
 ##'   \item{T2}{T2}
 ##'   \item{T3}{T3}
 ##'   \item{T4}{T4}
-##'   \item{DE_DATE}{DE_DATE}
-##'   \item{DIVE_DUR}{DIVE_DUR}
-##'   \item{MAX_DEP}{MAX_DEP}
-##'   \item{SURF_DUR}{SURF_DUR}
 ##'   \item{lat}{lat}
 ##'   \item{lon}{lon}
-##'   \item{ref}{ref}
 ##' ...
 ##' }
 ##' @source \url{http://www.imos.org.au}
